2eaac0e4109745784e765a86f936d8e982c5d110
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-18 00:18:15 +0100

    add command implemented

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import exitCommand from './lib/commands/exit-command.js';
 import lsCommand from './lib/commands/ls-command.js';
 import upCommand from './lib/commands/up-command.js';
 import cdCommand from './lib/commands/cd-command.js';
+import addCommand from './lib/commands/add-command.js';
 import userCommandsHandler from './lib/user-commands-handler.js';
 
 process.chdir(os.homedir());
 printWelcome(getUserNameFromArgs());
-userCommandsHandler.registerCommands([exitCommand, lsCommand, upCommand, cdCommand]);
+userCommandsHandler.registerCommands([exitCommand, lsCommand, upCommand, cdCommand, addCommand]);
 await userCommandsHandler.startHandleUserInput(() => printWorkingDirectory(process.cwd()));
 process.on('SIGINT', () => exitCommand.run([]));
