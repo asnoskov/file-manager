373128a8cba8f8a46ffdb9637de4f9e76a29a1a5
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-18 00:57:59 +0100

    rn command

---
+++ src/lib/commands/rn-command.js
@@ -0,0 +1,26 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import InvalidInputError from '../custom-errors/invalid-input-error.js';
+import { checkFileOrFolderExists } from '../fs-utils/check-file-or-folder-exists.js';
+
+const rnCommand = {
+    commandName: 'rn',
+    run: async (args) => {
+        if (args.length !== 2) {
+            throw new InvalidInputError();
+        }
+        const workingDirectory = process.cwd();
+        const pathToFile = path.resolve(workingDirectory, args[0].trim());
+        const newFileName = path.resolve(workingDirectory, args[1].trim());
+
+        if ((!await checkFileOrFolderExists(pathToFile))
+            || await checkFileOrFolderExists(newFileName)
+        ) {
+            throw new Error("FS operation failed");
+        }
+        
+        await fs.rename(pathToFile, newFileName);
+    }
+}
+
+export default rnCommand;
