05763ea3b7c16b8201efa2b53d05be2831b21f01
Aleksei Noskov <aleksei_noskov@epam.com>
2022-12-19 17:15:22 +0100

    hash command

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { exitCommand } from './lib/commands/common/index.js';
 import { cdCommand, lsCommand, upCommand } from './lib/commands/working-directory-navigation/index.js';
 import { addCommand, catCommand, cpCommand, rnCommand, rmCommand, mvCommand } from './lib/commands/file-operations/index.js';
 import { osCommand } from './lib/commands/os-info/index.js';
+import { hashCommand } from './lib/commands/hash-calculation/index.js';
 import userCommandsHandler from './lib/user-commands-handler.js';
 
 process.chdir(os.homedir());
 printWelcome(getUserNameFromArgs());
 userCommandsHandler.registerCommands([
     exitCommand, lsCommand, upCommand, cdCommand, addCommand,
-    rnCommand, rmCommand, catCommand, cpCommand, mvCommand, osCommand]);
+    rnCommand, rmCommand, catCommand, cpCommand, mvCommand, osCommand, hashCommand
+]);
 await userCommandsHandler.startHandleUserInput(() => printWorkingDirectory(process.cwd()));
 process.on('SIGINT', () => exitCommand.run([]));
---
+++ src/lib/commands/hash-calculation/index.js
@@ -0,0 +1,1 @@
+export { default as hashCommand } from './hash-command.js';
